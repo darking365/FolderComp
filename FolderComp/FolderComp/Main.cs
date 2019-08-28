074a367c019790cb9ec98d96734877b771db4527
Dkxiong <365687328@qq.com>
2019-08-28 11:20:50 +0800

    add file

---
+++ FolderComp/FolderComp/Main.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace FolderComp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            using (BackgroundWorker bg = new BackgroundWorker())
+            {
+                bg.DoWork += Bg_DoWork;
+                bg.RunWorkerCompleted += Bg_RunWorkerCompleted;
+                bg.RunWorkerAsync();
+            }
+        }
+
+        private void Bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Result!=null)
+            {
+                //generate the excel
+                string result = e.Result.ToString();
+                string filePath = Path.GetDirectoryName(result);
+
+                string lFolder = txt_lFolder.Text.Trim();
+                string rFolder = txt_rFolder.Text.Trim();
+                string outFolder = filePath;
+
+                string fileName = $"{lFolder}_{rFolder}".Replace(":","").Replace("\\","-");
+                var model = Util.DeserializeFile<bcreport>(result);
+                var lst = Util.GetResult(model);
+                string xls = Path.Combine(filePath, $"{fileName}.xlsx");
+                if (Util.Export(lst,xls))
+                {
+                    txt_outFolder.Text = filePath;
+                    lbl_fileName.Text = xls;
+                    btn_openfile.Enabled = true;
+                    //refresh the config file
+                    Util.UpdateAppConfig("lfolder", lFolder);
+                    Util.UpdateAppConfig("rfolder", rFolder);
+                    Util.UpdateAppConfig("ofolder", outFolder);
+                    MessageBox.Show("completed");
+                }
+            }
+        }
+
+        private void Bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string exeFolder=Util.GetAppPath("BCompare");
+            if (!exeFolder.IsValid())
+            {
+                MessageBox.Show("please make sure this computer has installed the software Beyond Compare.");
+                return;
+            }
+            string lFolder = txt_lFolder.Text.Trim();
+            string rFolder = txt_rFolder.Text.Trim();
+            string outFolder = txt_outFolder.Text.Trim();
+            if (!lFolder.IsValid())
+            {
+                MessageBox.Show("please confirm the left folder is correct.");
+            }else if (!rFolder.IsValid())
+            {
+                MessageBox.Show("please confirm the right folder is correct.");
+            }else if(!outFolder.IsValid())
+            {
+                MessageBox.Show("please confirm the output file folder is correct.");
+            }
+            else
+            {
+                var files= Directory.GetFiles(Path.Combine(Application.StartupPath,"scripts"));
+                if (files.Length == 0)
+                {
+                    MessageBox.Show("script does not exist");
+                    return;
+                }
+                string script = files[0];
+                outFolder = Path.Combine(outFolder, DateTime.Now.ToString("yyyyMMddHHmmss") + ".xml");
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName =Path.Combine(exeFolder,"BComp.exe"),
+                    UseShellExecute=false,
+                    CreateNoWindow=true,
+                    Arguments= $" /silent @\"{script}\" {lFolder} {rFolder} {outFolder} "
+                };
+
+                using(Process bc=new Process() { StartInfo=psi})
+                {
+                    try
+                    {
+                        bc.Start();
+                        e.Result = outFolder;
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btn_selL_Click(object sender, EventArgs e)
+        {
+            folder_left.ShowNewFolderButton = true;
+            if (folder_left.ShowDialog() == DialogResult.OK)
+            {
+                txt_lFolder.Text = folder_left.SelectedPath;
+            }
+        }
+
+        private void btn_selR_Click(object sender, EventArgs e)
+        {
+            folder_right.ShowNewFolderButton = true;
+            if (folder_right.ShowDialog() == DialogResult.OK)
+            {
+                txt_rFolder.Text = folder_right.SelectedPath;
+            }
+        }
+
+        private void btn_selO_Click(object sender, EventArgs e)
+        {
+
+            folder_output.ShowNewFolderButton = true;
+            if (folder_output.ShowDialog() == DialogResult.OK)
+            {
+                txt_outFolder.Text = folder_output.SelectedPath;
+            }
+        }
+
+        private void btn_openfile_Click(object sender, EventArgs e)
+        {
+            new System.Threading.Tasks.TaskFactory().StartNew(() =>
+            {
+                ProcessStartInfo psi = new ProcessStartInfo()
+                {
+                    FileName = "notepad",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    Arguments = lbl_fileName.Text
+                };
+                using(Process p=new Process { StartInfo = psi })
+                {
+                    p.Start();
+                }
+
+            });
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string lfolder = Util.GetAppConfig("lfolder");
+            string rfolder = Util.GetAppConfig("rfolder");
+            string ofolder = Util.GetAppConfig("ofolder");
+            txt_lFolder.Text = lfolder;
+            txt_rFolder.Text = rfolder;
+            txt_outFolder.Text = ofolder;
+        }
+    }
+}
---
+++ FolderComp/FolderComp/Util.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Win32;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+namespace FolderComp
+{
+    public class Util
+    {
+        /// <summary>
+        /// JSON序列化
+        /// </summary>
+        public static string JsonSerializer<T>(T t)
+        {
+            var ser = new DataContractJsonSerializer(typeof(T));
+            var ms = new MemoryStream();
+            ser.WriteObject(ms, t);
+            string jsonString = Encoding.UTF8.GetString(ms.ToArray());
+            ms.Close();
+            return jsonString;
+        }
+
+        /// <summary>
+        /// JSON反序列化
+        /// </summary>
+        public static T JsonDeserialize<T>(string jsonString)
+        {
+            var ser = new DataContractJsonSerializer(typeof(T));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            var obj = (T)ser.ReadObject(ms);
+            return obj;
+        }
+
+        /// <summary>
+        /// XML序列化某一类型到指定的文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="obj"></param>
+        /// <param name="type"></param>
+        public static void SerializeToXml<T>(string filePath, T obj)
+        {
+            try
+            {
+                using (System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath))
+                {
+                    XmlSerializer xs = new XmlSerializer(typeof(T));
+                    xs.Serialize(writer, obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static T DeserializeFile<T>(string filePath)
+        {
+            T result = default(T);
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                    result = (T)xmlSerializer.Deserialize(reader);
+                }
+            }
+            return result;
+        }
+
+        ///<summary> 
+        ///get value from config file
+        ///</summary> 
+        ///<param name="strKey"></param> 
+        ///<returns></returns> 
+        public static string GetAppConfig(string strKey)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key == strKey)
+                {
+                    return config.AppSettings.Settings[strKey].Value.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        ///<summary>  
+        ///update the config file  
+        ///</summary>  
+        ///<param name="newKey"></param>  
+        ///<param name="newValue"></param>  
+        public static void UpdateAppConfig(string newKey, string newValue)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            bool exist = false;
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key == newKey)
+                {
+                    exist = true;
+                }
+            }
+            if (exist)
+            {
+                config.AppSettings.Settings.Remove(newKey);
+            }
+            config.AppSettings.Settings.Add(newKey, newValue);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        /// <summary>
+        /// get the Beyond Compare install path
+        /// </summary>
+        /// <param name="appName"></param>
+        /// <returns></returns>
+        public static string GetAppPath(string appName)
+        {
+            string appPath = string.Empty;
+            try
+            {
+                string softPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+                RegistryKey regKey = Registry.LocalMachine;
+                RegistryKey regSubKey = regKey.OpenSubKey($"{softPath}\\{appName}.exe");
+                if (regSubKey == null)
+                {
+                    regKey = Registry.CurrentUser;
+                    regSubKey = regKey.OpenSubKey($"{softPath}\\{appName}.exe");
+                    if (regSubKey == null) return appPath;
+                }
+                var val = regSubKey.GetValue("");
+                if (val != null)
+                {
+                    appPath = Path.GetDirectoryName(val.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.Logger();
+            }
+            return appPath;
+        }
+
+        /// <summary>
+        /// export the data
+        /// </summary>
+        /// <param name="lst"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static bool Export(List<Level> lst,string fileName)
+        {
+            bool isSuccess = false;
+            XSSFWorkbook workBook = new XSSFWorkbook();  
+            XSSFSheet sheet = (XSSFSheet)workBook.CreateSheet();
+            //add header style
+            ICellStyle cellStyle = workBook.CreateCellStyle();
+            cellStyle.Alignment = HorizontalAlignment.Center;
+            cellStyle.VerticalAlignment = VerticalAlignment.Center;
+            IFont font = workBook.CreateFont();
+            font.Boldweight = (Int16)FontBoldWeight.Bold;
+            font.FontHeightInPoints = 18;
+            cellStyle.SetFont(font);
+            //header settings
+            IRow header = sheet.CreateRow(0);
+            header.HeightInPoints = 35;
+            List<string> headers = new List<string> { "Left Folder", "Right Folder" };
+            for(int m = 0; m < headers.Count; m++)
+            {
+                ICell cell = header.CreateCell(m*2);
+                cell.SetCellValue(headers[m]);
+                cell.CellStyle = cellStyle;
+                sheet.AddMergedRegion(new CellRangeAddress(0, 0, m*2, m*2+1));
+            }
+
+            IRow title = sheet.CreateRow(1);
+            title.HeightInPoints = 20;
+            cellStyle.Alignment = HorizontalAlignment.Left;
+            List<string> titles = new List<string> { "Path", "Filename", "Path", "FileName", "Comparision Result" };
+            for(int j = 0; j < titles.Count; j++)
+            {
+                ICell cell = title.CreateCell(j);
+                cell.SetCellValue(titles[j]);
+                cell.CellStyle = cellStyle;
+            }
+            #region body
+            for (int i = 0; i < lst.Count; i++)
+            {
+                sheet.AutoSizeColumn(i);
+                IRow body = sheet.CreateRow(i + 2);
+                string lpath = string.Empty;
+                string lfile = "NA";
+                string rpath = string.Empty;
+                string rfile = "NA";
+                string result = "NA";
+
+                if (lst[i].LeftNode != null)
+                {
+                    lpath = lst[i].LeftNode.Path;
+                    lfile = lst[i].LeftNode.FileName;
+                    result = lst[i].LeftNode.Result;
+                }
+                else
+                {
+                    lpath = lst[i].LtPath;
+                }
+                if (lst[i].RightNode != null)
+                {
+                    rpath = lst[i].RightNode.Path;
+                    rfile = lst[i].RightNode.FileName;
+                    result = lst[i].RightNode.Result;
+                }
+                else
+                {
+                    rpath = lst[i].RtPath;
+                }
+                switch (result)
+                {
+                    case "ltonly":
+                        result = $"Only exists in {lst[i].LeftNode.Path}";
+                        break;
+                    case "rtonly":
+                        result = $"Only exists in {lst[i].RightNode.Path}";
+                        break;
+                    default:
+                        result = "Text files are different";
+                        break;
+                }
+                body.CreateCell(0).SetCellValue(lpath);
+                body.CreateCell(1).SetCellValue(lfile);
+                body.CreateCell(2).SetCellValue(rpath);
+                body.CreateCell(3).SetCellValue(rfile);
+                body.CreateCell(4).SetCellValue(result);
+            } 
+            #endregion
+            try
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    workBook.Write(fs);  
+                    workBook.Close();  
+                }
+                isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                ex.Message.Logger();
+                workBook.Close();
+            }
+            return isSuccess;
+        }
+
+        public static List<Level> GetResult(bcreport report)
+        {
+            List<Level> lst = new List<Level>();
+            int layer = 0;
+            string preLtPath = string.Empty;
+            string preRtPath = string.Empty;
+            return ProcessFolderComp(report.foldercomp,lst, layer,preLtPath,preRtPath);            
+        }
+
+        private static List<Level> ProcessFolderComp(foldercomp fc,List<Level> lst,int layer,string preLtPath,string preRtPath)
+        {
+            if (fc != null)
+            {
+                if (fc.filecomps != null)
+                {
+                    layer++;
+                    preLtPath = layer==1?fc.ltpath:Path.Combine(preLtPath,fc.lt.name);
+                    preRtPath = layer==1?fc.rtpath:Path.Combine(preRtPath,fc.rt.name);
+
+                    foreach (var f in fc.filecomps)
+                    {
+                        Level level = new Level() { Layer = layer,LtPath=preLtPath,RtPath=preRtPath };
+                        if (f.lt != null)
+                        {
+                            Node lnode = new Node
+                            {
+                                Path=preLtPath,
+                                FileName=f.lt.name,
+                                Result=f.status
+                            };
+                            level.LeftNode = lnode;
+                        }
+                        if(f.rt != null)
+                        {
+                            Node rnode = new Node
+                            {
+                                Path = preRtPath,
+                                FileName = f.rt.name,
+                                Result = f.status
+                            };
+                            level.RightNode = rnode;
+                        }
+                        lst.Add(level);
+                    }
+                }
+                if (fc.FolderComp != null)
+                {
+                    ProcessFolderComp(fc.FolderComp, lst, layer, preLtPath, preRtPath);
+                }
+            }
+            return lst;
+        }
+    }
+
+    public static class StringExtention
+    {
+        public static bool IsValid(this string value)
+        {
+            bool isValid = false;
+            if (!string.IsNullOrEmpty(value.Trim()))
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+
+        public static async void Logger(this string msg)
+        {
+            string log = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{DateTime.Now.ToString("yyyyMMddHH")}.log");
+            using (FileStream fs = new FileStream(log, FileMode.Append))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    await sw.WriteLineAsync($"{DateTime.Now.ToString("yyyyMMddHHmmss")},info:{msg}");
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    [XmlRoot("bcreport")]
+    public class bcreport
+    {
+        [XmlAttribute("created")]
+        public string created { get; set; }
+        [XmlElement("foldercomp")]
+        public foldercomp foldercomp { get; set; }
+    }
+
+    public class foldercomp
+    {
+        [XmlElement("ltpath")]
+        public string ltpath { get; set; }
+        [XmlElement("rtpath")]
+        public string rtpath { get; set; }
+        [XmlElement("filters")]
+        public string filters { get; set; }
+        [XmlElement("foldercomp")]
+        public foldercomp FolderComp { get; set; }
+        [XmlElement("filecomp")]
+        public List<filecomp> filecomps { get; set; }
+        [XmlElement("lt")]
+        public lt lt { get; set; }
+        [XmlElement("rt")]
+        public rt rt { get; set; }
+    }
+
+    public class filecomp
+    {
+        [XmlAttribute("status")]
+        public string status { get; set; }
+        [XmlElement("lt")]
+        public lt lt { get; set; }
+        [XmlElement("rt")]
+        public rt rt { get; set; }
+    }
+
+    public class FileModel
+    {
+        [XmlElement("name")]
+        public string name { get; set; }
+        [XmlElement("size")]
+        public string size { get; set; }
+        [XmlElement("modified")]
+        public string modified { get; set; }
+        [XmlElement("attr")]
+        public string attr { get; set; }
+    }
+
+    public class lt:FileModel
+    {
+
+    }
+
+    public class rt:FileModel
+    {
+
+    }
+
+    public class Level
+    {
+        public int Layer { get; set; }
+        public string RtPath { get; set; }
+        public string LtPath { get; set; }
+
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+    }
+
+    public class Node
+    {
+        public string Path { get; set; }
+        public string FileName { get; set; }
+        public string Result { get; set; }
+    }
+}
